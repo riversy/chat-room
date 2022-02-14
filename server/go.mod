1e2c31cbf513b585f0a234446f19a9c2e87ac46a
Igor Goltsov <riversy@gmail.com>
2022-02-14 16:18:43 +0300

    Add a simple communication abilities between client and server

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/riversy/chat-room/server
 
 go 1.17
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- server/main.go
+++ server/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// reader
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// print out that message for clarity
+		fmt.Println(string(message))
+
+		err = conn.WriteMessage(messageType, []byte(fmt.Sprintf("%s from server", message)))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+// serveWs
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+
+	http.HandleFunc("/ws", serveWs)
+}
 
 func main() {
-	fmt.Println("Hello from Server!")
+	setupRoutes()
+	fmt.Println("Listen http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
 }
